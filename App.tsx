9a7f223013bd0216d44b535b21843f3f98c074c8
Yusuf Putra <yusufputra99@gmail.com>
2023-03-14 16:02:12 +0700

    feat: init scanner run perfect on android

---
+++ App.tsx
@@ -0,0 +1,132 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useEffect, useState} from 'react';
+
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {runOnJS} from 'react-native-reanimated';
+import {
+  Camera,
+  useCameraDevices,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Barcode, BarcodeFormat, scanBarcodes} from 'vision-camera-code-scanner';
+
+const getPermission = async () => {
+  const cameraPermission = await Camera.getCameraPermissionStatus();
+  const microphonePermission = await Camera.getMicrophonePermissionStatus();
+
+  return {
+    cameraPermission,
+    microphonePermission,
+  };
+};
+const requestPermission = async () => {
+  const cameraPermission = await Camera.requestCameraPermission();
+  const microphonePermission = await Camera.requestCameraPermission();
+
+  return {
+    cameraPermission,
+    microphonePermission,
+  };
+};
+const CameraComponent = () => {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [barcodes, setBarcodes] = useState<Barcode[]>([]);
+  useEffect(() => {
+    console.log('running');
+    getPermission().then(res => {
+      if (
+        res.cameraPermission === 'denied' ||
+        res.cameraPermission === 'not-determined' ||
+        res.microphonePermission === 'denied' ||
+        res.microphonePermission === 'not-determined'
+      ) {
+        requestPermission().then(result => {
+          setHasPermission(result.cameraPermission === 'authorized');
+        });
+      }
+    });
+  }, []);
+  const frameProcessor = useFrameProcessor(frame => {
+    'worklet';
+    const detectedBarcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE]);
+    console.log(detectedBarcodes?.[0]?.content?.data);
+    runOnJS(setBarcodes)(detectedBarcodes);
+  }, []);
+  const devices = useCameraDevices();
+  const device = devices.back;
+  if (!device || !hasPermission) {
+    return <Text style={styles.highlight}>loading</Text>;
+  }
+  return (
+    <>
+      {device && hasPermission && (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          frameProcessor={frameProcessor}
+        />
+      )}
+      {barcodes.map((barcode, idx) => (
+        <Text key={idx} style={styles.barcodeTextURL}>
+          {barcode.displayValue}
+        </Text>
+      ))}
+    </>
+  );
+};
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          height: '100%',
+        }}>
+        <CameraComponent />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  barcodeTextURL: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
