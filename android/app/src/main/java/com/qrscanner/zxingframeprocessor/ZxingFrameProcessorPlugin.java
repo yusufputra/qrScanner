922d3f736c0b2ab46979cd94fdea4dbbc314393e
Yusuf Putra <yusufputra99@gmail.com>
2023-03-17 16:12:28 +0700

    read multi qrcode

--- android/app/src/main/java/com/qrscanner/zxingframeprocessor/ZxingFrameProcessorPlugin.java
+++ android/app/src/main/java/com/qrscanner/zxingframeprocessor/ZxingFrameProcessorPlugin.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import androidx.camera.core.ImageProxy;
 
 import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.EnumMap;
 
 import com.facebook.react.bridge.WritableNativeArray;
 import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
@@ -20,10 +22,17 @@ import android.graphics.ImageFormat;
 import java.io.ByteArrayOutputStream;
 import android.graphics.Rect;
 import android.graphics.BitmapFactory;
+import com.google.zxing.multi.GenericMultipleBarcodeReader;
+import com.google.zxing.multi.MultipleBarcodeReader;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.DecodeHintType;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class ZxingFrameProcessorPlugin extends FrameProcessorPlugin {
-  private MultiFormatReader mReader = new MultiFormatReader();
+  private MultiFormatReader reader;
+  private MultipleBarcodeReader mReader;
   private Bitmap toBitmap(Image image) {
     Image.Plane[] planes = image.getPlanes();
     ByteBuffer yBuffer = planes[0].getBuffer();
@@ -46,40 +55,49 @@ public class ZxingFrameProcessorPlugin extends FrameProcessorPlugin {
 
     byte[] imageBytes = out.toByteArray();
     return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-}
+  }
+
   @Override
   public Object callback(ImageProxy image, Object[] params) {
-      Log.d("IMAGESTATE",image.toString());
     Image images = image.getImage();
     Bitmap bitmap = toBitmap(images);
     int width = bitmap.getWidth();
     int height = bitmap.getHeight();
     int[] pixels = new int[width * height];
     bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
-    // image.getPlanes();
-
-    Result result = null;
+    Result[] results = null;
     try {
-        result = mReader.decode(new BinaryBitmap(new HybridBinarizer(new RGBLuminanceSource(width, height, pixels))));
+        results = mReader.decodeMultiple(new BinaryBitmap(new HybridBinarizer(new RGBLuminanceSource(width, height, pixels))));
     } catch (Exception e) {
         e.printStackTrace();
     }
     WritableNativeArray resultArray = new WritableNativeArray();
-    if (result != null) {
-        WritableNativeArray resultData = new WritableNativeArray();
-        resultData.pushString(result.getBarcodeFormat().toString());
-        resultData.pushString(result.getText());
+    if (results != null) {
+        for (Result result : results) {
+            WritableNativeArray resultData = new WritableNativeArray();
+            resultData.pushString(result.getBarcodeFormat().toString());
+            resultData.pushString(result.getText());
 
-        resultArray.pushArray(resultData);
-
+            resultArray.pushArray(resultData);
+        }
     }
     return resultArray;
-    // // code goes here
-    // WritableNativeArray array = new WritableNativeArray();
-    // return array;
   }
-
+  private static Map<DecodeHintType, Object> getHints() {
+    Map<DecodeHintType, Object> hints = new EnumMap<>(DecodeHintType.class);
+    List<BarcodeFormat> formats = new ArrayList<>();
+    formats.add(BarcodeFormat.QR_CODE);
+    formats.add(BarcodeFormat.CODE_39);
+    formats.add(BarcodeFormat.CODE_128);
+    hints.put(DecodeHintType.POSSIBLE_FORMATS, formats);
+    hints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+    hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+    return hints;
+}
   public ZxingFrameProcessorPlugin() {
     super("detectBarcodes");
+    reader = new MultiFormatReader();
+    reader.setHints(getHints());
+    mReader = new GenericMultipleBarcodeReader(reader);
   }
 }
